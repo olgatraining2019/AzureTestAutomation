50a11f249f6b9f36e850b8df0f9d2cb9567fc639
OlgaI. <119903704+olgatraining2019@users.noreply.github.com>
2026-03-17 13:48:23 -0700

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,31 +1,29 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserTest {
 
-	
 	@Test
-	public void getData()
-	{
+	public void getData() {
 		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://olgatraining2026-amecemgrdcgee9ey.canadacentral-01.azurewebsites.net/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
-		driver.close();
-	
-		
-		
+		// Downloads / selects ChromeDriver that matches the installed Chrome (local + CI)
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+		try {
+			driver.get("https://olgatraining2026-amecemgrdcgee9ey.canadacentral-01.azurewebsites.net/");
+			String text = driver.findElement(By.cssSelector("h1")).getText();
+			System.out.println(text);
+			Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		} finally {
+			driver.quit();
+		}
 	}
 }
